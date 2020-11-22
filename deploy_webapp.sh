bfc9abeba3ed3bfe3f1e842dbeccb9b070e1ee92
stephansmit <stephansmit@hotmail.com>
2020-11-22 12:22:15 +0100

    Update deploy_webapp.sh

--- deploy_webapp.sh
+++ deploy_webapp.sh
@@ -4,11 +4,23 @@ APP_PLANE_NAME="${APP_NAME}plan"
 RESOURCE_GROUP=flaskPythonApp
 LOCATION=northeurope
 
+
 # Create a Resource Group
-az group create -–name $RESOURCE_GROUP –-location $LOCATION
+az group create \
+        --location northeurope \
+        --name $RESOURCE_GROUP
 
 # Create an App Service Plan
-az appservice plan create -–name $APP_PLAN_NAME -–resource-group $RESOURCE_GROUP -–location $LOCATION –is-linux –sku B1
+az appservice plan create \
+        --resource-group $RESOURCE_GROUP \
+        --name $APP_PLAN_NAME \
+        --location $LOCATION \
+        --is-linux -–sku B1
 
 # Create a Web App
-az webapp create –-name $APP_NAME –plan $APP_PLAN_NAME -–resource-group $RESOURCE_GROUP -–runtime "python|3.6" --startup-file startup.txt
+az webapp create \
+        --name $APP_NAME \
+        --plan $APP_PLAN_NAME \
+        --resource-group $RESOURCE_GROUP \
+        --runtime "python|3.6" \
+        --startup-file startup.txt
