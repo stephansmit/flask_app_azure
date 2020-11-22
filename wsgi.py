e86a1ff1bfd233f84c4171ca4bc93280be213af0
stephansmit <stephansmit@hotmail.com>
2020-11-22 15:32:00 +0100

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
-from flask_app_azure import app
+from flask import Flask
+
+@app.route("/")
+def hello():
+    return "<h1 style='color:blue'>Hello There!</h1>"
 
 if __name__ == "__main__":
-    app.run()
+    app = Flask(__name__)
+    app.run(host='0.0.0.0')
